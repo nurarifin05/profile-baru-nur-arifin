98f40ed96257574ade2dbfcc0c10e6436063134f
nurarifin05 <muhamadnurarifin05@gmail.com>
2021-02-19 14:24:24 +0700

    halaman1

---
+++ src/components/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import Typed from "react-typed";
+
+
+const Header = () => {
+    return (
+        <div className="header-wraper">
+            <div className="main-info">
+                <h1>Under construction</h1>
+                <Typed
+                className="typed-text"
+                strings={["Web Development", "IoT Development", "RPA Development", "Penulisan Skripsi", "Penulisan Jurnal"]}
+                typeSpeed={40}
+                backSpeed={60}
+                loop
+
+                />
+                <a href="https://t.me/muhamadnurarifin" className="btn-main-offer">contact me</a>
+            </div>
+
+            
+        </div>
+    )
+}
+
+export default Header
---
+++ src/components/Navbar.js
@@ -0,0 +1,58 @@
+import React from "react";
+import logo1 from "../images/logo2.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {faBars} from "@fortawesome/free-solid-svg-icons";
+
+
+const Navbar = () => {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+            <div className="container">
+                <a className="navbar-brand" href="#"><img className="logo1" src={logo1} alt="logo1..." /></a>
+
+                <button className="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+
+                    <FontAwesomeIcon icon={faBars} style={{color: "#fff"}} />
+                </button>
+
+                <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul className="navbar-nav ml-auto">
+                        <li className="nav-item active">
+                            <a className="nav-link" aria-current="page" href="#">Home 
+                            <span className="sr-only"></span></a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">About me</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">Services</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">How Work</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">portofolio</a>
+                        </li>
+
+                        <li className="nav-item">
+                            <a className="nav-link" href="#">contact</a>
+                        </li>
+
+
+                    </ul>
+
+
+                </div>
+            </div>
+        </nav>
+
+    )
+}
+
+export default Navbar;
